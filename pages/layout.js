6e6595390285615197e5cb9326802a6f64d13a39
Bruno G. Tavares <devbruno78@gmail.com>
2016-08-19 03:39:45 -0400

    added some buttons and adjust routes

--- pages/layout.js
+++ pages/layout.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { Link } from "react-router";
+
 export default class Layout extends React.Component {
+  navigate() {
+    this.props.history.replaceState(null, "/");
+  }
+
   render() {
     return (
       <div>
         <h1>Welcome to My Page</h1>
-        <link to="about" >about</link>
+        {this.props.children}
+        <Link to="about"><button class="btn btn-success">about</button></Link>
+        <Link to="contact"><button class="btn btn-success">contact</button></Link>
+        <button onClick={this.navigate.bind(this)}>Featured</button>
       </div>
     );
   }
